7fddf2857d56942dd240c4a2e10b3000d0ddb65c
Jadson Santos <jadsondossantos5@gmail.com>
2024-04-19 17:42:46 -0300

    fix: fix database connection

--- src/database/mongoose.database.js
+++ src/database/mongoose.database.js
@@ -1,13 +1,16 @@
+const { config } = require("dotenv");
 const mongoose = require("mongoose");
 
-const DB_CONNECTION_STRING = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@taskmanagercluster.oi0vhvd.mongodb.net/?retryWrites=true&w=majority&appName=taskmanagercluster`;
+config();
+
+const DB_CONNECTION_STRING = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@taskmanagercluster.oi0vhvd.mongodb.net/?retryWrites=true&w=majority`;
 
 const connectToDatabase = async () => {
     try {
         await mongoose.connect(DB_CONNECTION_STRING);
         console.log("Database connected successfully");
     } catch (error) {
-        console.error("Error connecting to the database", error);
+        console.error("Error connecting to the database: ", error);
     }
 };
 
